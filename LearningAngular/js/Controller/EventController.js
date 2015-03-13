b9129d0c1cc2d445ad85463716d9adb0a5b55736
Sagar Thapa <sagarthapa_9@yahoo.com>
2015-03-13 17:03:59 +0000

    Updated filters and sorting

--- LearningAngular/js/Controller/EventController.js
+++ LearningAngular/js/Controller/EventController.js
@@ -5,6 +5,7 @@ var LearnAngJs = angular.module("LearnAngJs");
 LearnAngJs.controller('EventController',
      function EventController($scope) {
 
+         $scope.sortorder = "name";
          $scope.event = {
 
              name: 'Angular Tutorial',
@@ -18,21 +19,21 @@ LearnAngJs.controller('EventController',
              imageUrl: '/Img/cdnLogo.jpg',
              sessions:[
                   {
-                      name: 'session1',
+                      name: 'Beginners',
                       Duration: '2 Hours',
                       Level: 1,
                       Price:25,
                       totalVote:0
                   },
                   {
-                       name:'session2',
+                       name:'Intermidiate',
                        Duration: '4 Hours',
                        Level: 2,
                        Price: 50,
                        totalVote:0
                    },
                   {
-                      name:'session3',
+                      name:'Advance',
                       Duration: '10 Hours',
                       Level: 3,
                       Price: 100,
